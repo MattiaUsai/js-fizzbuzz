adc7415d5a289cb070b3e47f40687384ff6d44cf
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-02 15:20:44 +0200

    BONUS

--- main.js
+++ main.js
@@ -9,18 +9,27 @@
 // -- ALTRIMENTI STAMPA IL NUMERO  
 
 for (let x = 0; x <= 100 ; x++) {
-
+    const div = document.getElementById("contenitore")
+    
     if ( x % 15 == 0) {
         console.log  ("FIZZBUZZ");
+        const element = `<div class="col-2 m-2 text-center py-5 fs-3 text-bg-info">FIZZBUZZ</div>`
+        div.innerHTML += element;
     } 
     else if (x % 3 ===0){
         console.log("FIZZ");
+        const element = `<div class="col-2 m-2 text-center py-5 fs-3 text-bg-danger">FIZZ</div>`
+        div.innerHTML += element;
     }
     else if ( x % 5  ===0){
         console.log("BUZZ");
+        const element = `<div class="col-2 m-2 text-center py-5 fs-3 text-bg-success">BUZZ</div>`
+        div.innerHTML += element;
     }
     else {
         console.log(x);
+        const element = `<div class="col-2 m-2 text-center py-5 fs-3 text-bg-primary">${x}</div>`
+        div.innerHTML += element;
     }
     
 }
